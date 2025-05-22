6c2f06111c92911266c6357c3f7704a8321dfb02
Bhavya Garg <bebhavyagarg@gmail.com>
2025-05-22 09:22:13 +0530

    Fixed user type

--- user-access-backend/src/middlewares/authMiddleware.ts
+++ user-access-backend/src/middlewares/authMiddleware.ts
@@ -1,36 +1,45 @@
-import { RequestHandler, Request } from 'express';
-import jwt from 'jsonwebtoken';
-import { UserDao } from '../dao/userDao';
+import { RequestHandler, Request } from "express";
+import jwt from "jsonwebtoken";
+import { UserDao } from "../dao/userDao";
+
+import { User } from "../../src/entities/User";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: User;
+  }
+}
 
 const userDao = new UserDao();
 
 export const validateUser: RequestHandler = async (req: Request, res, next) => {
-    try {
-        const token  = req.headers.token as string;
-        if(!token)
-        {
-            throw new Error("Authentication token missing");
-        }
-
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as {username:string};
-
-        const user = await userDao.findUserByUsername(decodedToken.username);
-
-        if(!user)
-        {
-            throw new Error("User not found");
-        }
-
-        req.user = user;
-
-        next();
-    } catch (error) {
-        if (error instanceof Error) {
-            res.status(401).json({ 
-                message: error.message
-            });
-            return;
-        }
-        next(error);
+  try {
+    const token = req.headers.token as string;
+    if (!token) {
+      throw new Error("Authentication token missing");
     }
+
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET_KEY as string
+    ) as { username: string };
+
+    const user = await userDao.findUserByUsername(decodedToken.username);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(401).json({
+        message: error.message,
+      });
+      return;
+    }
+    next(error);
+  }
 };
